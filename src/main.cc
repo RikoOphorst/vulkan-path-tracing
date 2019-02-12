910e5bd92ee24ddd005baa7f703fe11745fb7410
Riko Ophorst <ophorst.riko@gmail.com>
2019-02-12 16:12:24 +0100

    Debug Messenger added

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,54 @@
+//
+//
+//
+
+const std::vector<const char*> vulkan_instance_extensions = {
+  "VK_KHR_surface",
+  "VK_KHR_win32_surface",
+  "VK_EXT_debug_utils"
+};
+
+const std::vector<const char*> vulkan_instance_layers = {
+  "VK_LAYER_LUNARG_standard_validation"
+};
+
+static VKAPI_ATTR VkBool32 VKAPI_CALL VulkanDebugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT message_severity, VkDebugUtilsMessageTypeFlagsEXT message_type, const VkDebugUtilsMessengerCallbackDataEXT* callback_data, void* user_data)
+{
+  std::cerr << "Vk: " << callback_data->pMessage << std::endl;
+  return VK_FALSE;
+}
+
+VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger) {
+  auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+  if (func != nullptr) {
+    return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+  }
+  else {
+    return VK_ERROR_EXTENSION_NOT_PRESENT;
+  }
+}
+
+void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator) {
+  auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+  if (func != nullptr) {
+    func(instance, debugMessenger, pAllocator);
+  }
+}
+
+//
+//
+//
+//
+//
+//
+//
+//
+
 int main(int argc, char** argv)
 {
   GLFWwindow* window = nullptr;
   VkInstance instance;
+  VkDebugUtilsMessengerEXT debug_messenger = {};
 
   // GLFW init & window
   {
@@ -21,21 +68,29 @@ int main(int argc, char** argv)
     app_info.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
     app_info.pEngineName = "No Engine";
     app_info.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-    app_info.apiVersion = VK_API_VERSION_1_0;
+    app_info.apiVersion = VK_API_VERSION_1_1;
 
     VkInstanceCreateInfo create_info = {};
     create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
     create_info.pApplicationInfo = &app_info;
-    
-    uint32_t glfw_extension_count = 0;
-    const char** glfw_extensions;
-
-    glfw_extensions = glfwGetRequiredInstanceExtensions(&glfw_extension_count);
-
-    create_info.enabledExtensionCount = glfw_extension_count;
-    create_info.ppEnabledExtensionNames = glfw_extensions;
+    create_info.enabledExtensionCount = static_cast<uint32_t>(vulkan_instance_extensions.size());
+    create_info.ppEnabledExtensionNames = vulkan_instance_extensions.data();
+    create_info.enabledLayerCount = static_cast<uint32_t>(vulkan_instance_layers.size());
+    create_info.ppEnabledLayerNames = vulkan_instance_layers.data();
 
     VkResult result = vkCreateInstance(&create_info, nullptr, &instance);
+  }
+
+  // Debug Messenger
+  {
+    VkDebugUtilsMessengerCreateInfoEXT create_info = {};
+    create_info.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+    create_info.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+    create_info.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+    create_info.pfnUserCallback = VulkanDebugCallback;
+    create_info.pUserData = nullptr; // Optional
+
+    CreateDebugUtilsMessengerEXT(instance, &create_info, nullptr, &debug_messenger);
   }
 
   // Main loop
@@ -48,6 +103,8 @@ int main(int argc, char** argv)
 
   // Shutdown
   {
+    DestroyDebugUtilsMessengerEXT(instance, debug_messenger, nullptr);
+
     vkDestroyInstance(instance, nullptr);
 
     glfwDestroyWindow(window);
